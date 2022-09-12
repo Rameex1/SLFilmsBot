0bc6e57994f10e5d14a92326c313f894d79bb6de
Hansaka Anuhas <113321165+SLHansakaAnuhas@users.noreply.github.com>
2022-09-12 07:26:07 +0530

    Add files via upload

---
+++ Script.py
@@ -0,0 +1,43 @@
+class script(object):
+    START_TXT = """Hello {} 👋
+I Can Provide Films 🥳
+Just Add Me To Your Groups As Admin 🤩"""
+    HELP_TXT = """Here Is The Help For My Commands"""
+    ABOUT_TXT = """★ My Name: {}
+★ Bot Server: Heroku
+★ Database: MongoDB
+★ Creator: <a href=https://t.me/Hansaka_Anuhas>Hansaka Anuhas</a>"""
+    MANUELFILTER_TXT = """Help: <b>Filters</b>
+    
+<b>Commands and Usage:</b>
+• /filter - Add a filter in chat
+• /filters - List all the filters of a chat
+• /del - Delete a specific filter in chat
+• /delall - Delete the whole filters in a chat"""
+    BUTTON_TXT = """Help: <b>Buttons</b>
+
+<b>URL Buttons:</b>
+<code>[Button Text](buttonurl:https://t.me/SL_Films_Bot)</code>
+
+<b>Alert Buttons:</b>
+<code>[Button Text](buttonalert:This is an alert message)</code>"""
+    CONNECTION_TXT = """Help: <b>Connections</b>
+
+<b>Commands and Usage:</b>
+• /connect - Connect a particular chat to your PM
+• /disconnect - Disconnect from a chat
+• /connections - List all your connections"""
+    STATUS_TXT = """★ Total Files: <code>{}</code>
+★ Total Users: <code>{}</code>
+★ Total Chats: <code>{}</code>
+★ Used Storage: <code>{}</code>
+★ Used Storage: <code>{}</code>"""
+    LOG_TEXT_G = """#NewGroup
+Group = {}(<code>{}</code>)
+Total Members = <code>{}</code>
+Added By - {}
+"""
+    LOG_TEXT_P = """#NewUser
+ID - <code>{}</code>
+Name - {}
+"""
